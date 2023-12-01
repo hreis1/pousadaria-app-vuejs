22b63f33c1688af05978df137670c5a6c290e050
Paulo Henrique <hreispaulo1@gmail.com>
2023-12-01 01:52:56 -0300

    verifica disponibilidade

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const app = Vue.createApp({
     data() { 
         return {
+            searchTerm: '',
+            
             inns: [],
-            inn : {},
             rooms: [],
-            room: {},
-            message: '',
+            
+            selectedInn: null,
+            selectedRoom: {},
+
+            checkin: '',
+            checkout: '',
+            number_of_guests: '',
+            
+            errorMessages: '',
+            showInns: true,
         }
     },
     methods: {
@@ -13,26 +22,54 @@ const app = Vue.createApp({
             const response = await fetch('http://localhost:3000/api/v1/inns')
             this.inns = await response.json()
         },
-        async getInn(id) {
-            const response = await fetch(`http://localhost:3000/api/v1/inns/${id}`)
-            const inn = await response.json()
-            this.inn = inn
-            this.inn.pets_allowed = inn.pets_allowed ? 'Sim' : 'Não'
-            document.getElementById('inn-details').hidden = false
-            this.showRooms(id)
-        },
-        async showRooms(id) {
-            try {
-                const response = await fetch(`http://localhost:3000/api/v1/inns/${id}/rooms`);
-                const rooms = await response.json();
-                this.rooms = rooms;
-                document.getElementById('rooms').hidden = false;
-            } catch (error) {
+        async search() {
+            if (this.searchTerm) {
+              try {
+                const response = await fetch(`http://localhost:3000/api/v1/inns/?trade_name=${this.searchTerm}`);
+                data = await response.json();
+                this.inns = data;
+                this.hideInnDetails();
+              } catch (error) {
                 console.error(error);
+                }
             }
         },
-
-        async checkRoom(inn_id, id, room) {
+        async showInnDetails(inn) {
+            const response = await fetch(`http://localhost:3000/api/v1/inns/${inn.id}`)
+            this.selectedInn = await response.json()
+            this.showInns = false;
+            await this.getRooms(inn)
+        },
+        async getRooms(inn) {
+            const response = await fetch(`http://localhost:3000/api/v1/inns/${inn.id}/rooms`)
+            this.rooms = await response.json()
+        },
+        async checkAvailability(room) {
+            const response = await fetch(`http://localhost:3000/api/v1/inns/${room.inn_id}/rooms/${room.id}/check?checkin=${this.checkin}&checkout=${this.checkout}&number_of_guests=${this.number_of_guests}`)
+            const data = await response.json()
+            console.log(data);
+            this.errorMessages = ''
+            if (response.ok && !data.mensagem) {
+                this.selectedRoom = room
+                this.selectedRoom.available = true
+                this.selectedRoom.checkin = this.checkin
+                this.selectedRoom.checkout = this.checkout
+                this.selectedRoom.number_of_guests = this.number_of_guests
+                this.selectedRoom.price = data.price
+            } else {
+                this.selectedRoom = room
+                this.errorMessages = data.mensagem
+            }
+            this.checkin = '',
+            this.checkout = '',
+            this.number_of_guests = ''
+        },
+        showForm(room) {
+            this.selectedRoom = room
+        },
+        hideInnDetails() {
+            this.selectedInn = null;
+            this.showInns = true;
         }
     },
     async mounted() {
